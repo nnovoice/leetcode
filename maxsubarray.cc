a94bc638d135431a18f961788a5628f603b87142
raghav <raghav@watchy.in>
2017-06-01 14:22:38 +0530

    Based on Kadane's algo. Gives WA in case of an array with only -ve elements or if the sum goes below 0

---
+++ maxsubarray.cc
@@ -0,0 +1,58 @@
+/*
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+*/
+
+#include <vector>
+#include <climits>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max = INT_MIN;
+        int n = nums.size();
+        int acc = 0;
+        for (int i = 0; i < n; ++i) {
+        	if ((acc + nums[i]) <= 0) acc = 0;
+        	else acc += nums[i];
+        	if (max < acc) max = acc;
+        }
+        return max;
+    }
+};
+
+void test0() {
+	int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+	std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+	Solution sol;
+	int res = sol.maxSubArray(v);
+	assert (res == 6);
+}
+
+void test1() {
+	int arr[] = {1,-2,2,3,-2,5,7};
+	std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+	Solution sol;
+	int res = sol.maxSubArray(v);
+	assert (res == 15);
+}
+
+void test2() {
+	int arr[] = {-2};
+	std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+	Solution sol;
+	int res = sol.maxSubArray(v);
+	assert (res == -2);
+}
+
+int main() {
+	test0();
+	test1();
+	test2();
+	return 0;
+}
