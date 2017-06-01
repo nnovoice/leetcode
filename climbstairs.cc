7c5db7801652ae0746af372c32bbf1e6f7a4b749
raghav <raghav@watchy.in>
2017-06-01 10:51:30 +0530

    TLE for the recursive version :-)

--- climbstairs.cc
+++ climbstairs.cc
@@ -16,6 +16,13 @@ public:
         }
         return f1;
     }
+    int climbStairsRecursive (int n) {
+
+    	if (n <= 0) return 0;
+    	if (n == 1) return 1;
+    	if (n == 2) return 2;
+    	return climbStairsRecursive(n - 2) + climbStairsRecursive (n - 1);
+    }
 };
 
 void test0() {
@@ -36,9 +43,23 @@ void test2() {
 	assert (res == 3);
 }
 
+void test0_recursive() {
+	Solution sol;
+	int res = sol.climbStairsRecursive(3);
+	assert (res == 3);
+}
+
+void test1_recursive() {
+	Solution sol;
+	int res = sol.climbStairsRecursive(10);
+	assert (res == 89);
+}
+
 int main() {
 	test0();
 	test1();
 	test2();
+	test0_recursive();
+	test1_recursive();
 	return 0;
 }
