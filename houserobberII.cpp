b3c79c74bd87cf75526a52464cff6475667a1fcc
raghav <raghav@watchy.in>
2017-06-03 09:32:05 +0530

    AC. Handled edge cases (lengths of 0,1) in the parent caller.

--- houserobberII.cpp
+++ houserobberII.cpp
@@ -5,47 +5,38 @@
 using namespace std;
 
 class Solution {
-public:
-    int rob(vector<int>& nums) {
-        /* there are only 2 cycles to consider in case of even number of elements
-           0, 2, 4, 6
-           1, 2, 5, 7
-           in case of odd number of elements
-           0, 2, 4 (6 becomes neighbour of 2)
-           1, 3, 5 (7%7 is 0 which is neighbour of 1)
-           2, 4, 6 (8%7 is 1 which is the neighbour of 2)
-		*/        
-        int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        int cycles = (n % 2 == 0) ? 2 : 3;
-        int maxloot = 0, loot = 0;
-        stack<int> left;
-        stack<int> right;
-        int l = 0, r = 0;
-        for (int i = 0; i < cycles; ++i) {
-        	// need to do this to wrap the -ve index back to +ve
-        	l = (n + (i - 2) % n) % n; 
-        	r = (i + 2) % n;
-        	while (1) {
-        		if (cycles == 3 && (((r + 1) % n) == i)) break;
-        		if (cycles == 2 && r == i) break;
-        		cout << "cycles=" << cycles << " n=" << n << " i=" << i << " l=" << l << " r=" << r << endl;
-        		left.push(l);
-        		right.push(r);
-        		l = (n + (l - 2) % n) % n;
-        		r = (r + 2) % n;
-        	}
-        	int lloot = nums[i], rloot = nums[i];
-        	while (left.empty() == false) {
-        		lloot += nums[left.top()];
-        		rloot += nums[right.top()];
-        		left.pop();
-        		right.pop();
-        	}
-        	maxloot = max(maxloot, max(lloot, rloot));
+private:
+	int robOriginal(vector<int>& nums, int m, int n) {
+        vector<int> loots (nums.size());
+
+        int maxloot = 0;
+        int idx1 = 0, idx2 = 0, idx3 = 0;
+        int l2 = 0, l3 = 0;
+        int m1 = 0, m2 = 0;
+        for (int i = m; i <= n; ++i) {
+        	idx2 = i - 2;
+        	idx3 = i - 3;
+        	
+        	l2 = (idx2 >= m) ? loots[idx2] : 0;
+        	l3 = (idx3 >= m) ? loots[idx3] : 0;
+        	
+        	loots[i] = nums[i] + max(l2, l3);
+        	
+        	idx1 = i - 1;
+
+        	m1 = (idx1 >= m) ? loots[idx1] : 0;
+        	m2 = loots[i];
+        	maxloot = max(m1, m2);
         }
         return maxloot;
+    }
+public:
+    int rob(vector<int>& nums) {
+    	if (nums.size() == 0) return 0;
+    	if (nums.size() == 1) return nums[0];
+        int m1 = robOriginal(nums, 0, nums.size() - 2);
+        int m2 = robOriginal(nums, 1, nums.size() - 1);
+        return max(m1, m2);
     }
 };
 
@@ -82,6 +73,14 @@ void test3() {
 	assert (res == 1);
 }
 
+void test3_0() {
+	int arr[] = {1,2,3};
+	std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+	Solution sol;
+	int res = sol.rob(v);
+	assert (res == 3);
+}
+
 void test4() {
 	int arr[] = {1,3,2};
 	std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
@@ -111,6 +110,7 @@ void test7() {
 	std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
 	Solution sol;
 	int res = sol.rob(v);
+	cout << "test7" << res << endl;
 	assert (res == 2);
 }
 
@@ -130,11 +130,15 @@ void test9() {
 	assert (res == 16);
 }
 
+
+
 int main() {
+	//test7();
 	test0();
 	test1();
 	test2();
 	test3();
+	test3_0();
 	test4();
 	test5();
 	test6();
