812f333d3ca37c3a4343e85dc6a62fd58d77fd8f
raghav <raghav@watchy.in>
2017-06-14 14:44:32 +0530

    AC. Submitted directly on Leetcode. So, this code does not have tests in it. TBD

---
+++ lowest-common-ancestor-BST.cc
@@ -0,0 +1,41 @@
+/*
+Leetcode 235. Lowest Common Ancestor of a Binary Search Tree
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+*/
+#include <vector>
+#include <stack>
+#include <climits>
+#include <utility>
+#include <cstdlib>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+*/
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) return NULL;
+        if (p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
+
+int main() {
+	return 0;
+}
