76de9857b9c84a5a728220762e0ec70bb3b64d05
raghav <raghav@watchy.in>
2017-06-15 15:55:53 +0530

    AC, but the code is very bad. unreadable.

---
+++ find-min-in-rotated-array.cc
@@ -0,0 +1,124 @@
+/*
+Leetcode 153. Find Minimum in Rotated Sorted Array
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+*/
+#include <vector>
+#include <stack>
+#include <climits>
+#include <utility>
+#include <cstdlib>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+
+class Solution {
+private:
+    int getElementUsingBS(vector<int>& nums, int low, int high) {
+    int mid = 0, ml = 0, mr = 0;
+    if (low == high) return nums[low];
+    if (low < high) {
+        mid = low + (high - low) / 2;
+        ml = mid - 1;
+        mr = mid + 1;
+        if (ml < 0) return (nums[mid] < nums[mr]) ? nums[mid] : nums[mr];
+        if (mr == nums.size()) return (nums[mid] < nums[ml]) ? nums[mid] : nums[ml];
+        if (nums[ml] > nums[mid] && nums[mid] < nums[mr]) return nums[mid];
+        
+        int lmin = getElementUsingBS(nums, low, mid - 1);
+        int rmin = getElementUsingBS(nums, mid + 1, high);
+        return min(lmin, rmin);
+    }
+    return nums[high];
+    
+}
+public:
+int findMin(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) return INT_MIN;
+    return getElementUsingBS(nums, 0, n - 1);
+}
+};
+/* Ignore this test
+void test0() {
+    Solution sol;
+    int arr[] = {};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 0);
+}*/
+
+void test1() {
+    Solution sol;
+    int arr[] = {-1};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == -1);
+}
+
+void test1_1() {
+    Solution sol;
+    int arr[] = {5};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 5);
+}
+
+void test2() {
+    Solution sol;
+    int arr[] = {1,2,3,4,5};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 1);
+}
+
+void test3() {
+    Solution sol;
+    int arr[] = {1,3,5,7,9,11};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 1);
+}
+
+void test4() {
+    Solution sol;
+    int arr[] = {7,9,11,1,3,5};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 1);
+}
+
+void test5() {
+    Solution sol;
+    int arr[] = {4,6,8,10,0,2};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 0);
+}
+
+void test6() {
+    Solution sol;
+    int arr[] = {10,8,6,4,2,0};
+    std::vector<int> v (arr, arr + (sizeof(arr)/sizeof(int)));
+    int res = sol.findMin(v);
+    assert (res == 0);
+}
+
+int main() {
+    //test0();
+    test1();
+    test1_1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    return 0;
+}
