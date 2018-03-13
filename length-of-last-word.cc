c6851463dd1739e17b3a37a2bec2da406f8e1579
nnovoice <n.raghavendran@gmail.com>
2018-03-13 22:27:02 +0530

    WA for 'a '. Assumption was wrong...last word needs to skip the trailing spaces

---
+++ length-of-last-word.cc
@@ -0,0 +1,81 @@
+/*
+58. Length of Last Word
+
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+*/
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cassert>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        int len = 0;
+        for (int i = n - 1; i >= 0 && s[i] != ' '; --i) {
+            ++len;
+        }
+        return len;
+    }
+};
+
+void test1() {
+    Solution s;
+    string str = "Hello, World";
+    int len = s.lengthOfLastWord(str);
+    assert(len == 5);
+}
+
+void test2() {
+    Solution s;
+    string str = "Hello, ";
+    int len = s.lengthOfLastWord(str);
+    assert(len == 0);
+}
+
+void test3() {
+    Solution s;
+    string str = "";
+    int len = s.lengthOfLastWord(str);
+    assert(len == 0);
+}
+
+void test4() {
+    Solution s;
+    string str = "Hello,World";
+    int len = s.lengthOfLastWord(str);
+    assert(len == 11);
+}
+
+void test5() {
+    Solution s;
+    string str = " Hello,";
+    int len = s.lengthOfLastWord(str);
+    assert(len == 6);
+}
+
+
+int main()
+{
+    test1();
+    test2(); 
+    test3();
+    test4();
+    test5();
+    return 0;
+}
